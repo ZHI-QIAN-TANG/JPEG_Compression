05f4727d73f2f0d6918b2e846414b9b72a1c76d7
Eric Chen <eric5552120@yahoo.com.tw>
2024-10-18 04:17:17 +0800

    no message

--- main.py
+++ main.py
@@ -299,7 +299,7 @@ def write_jpeg_file(encoded_data, output_file):
 '''
 
 def main():    
-    image_path = "test511x340.jpeg"
+    image_path = "test2.jpg"
     output_jpeg = "output1.jpg"
 
     # Y_AC_codebook_bytes,UV_AC_codebook_bytes,Y_DC_codebook_bytes,UV_DC_codebook_bytes,encoded_bytes_Y_DC,encoded_bytes_U_DC,encoded_bytes_V_DC,encoded_bytes_Y_AC,encoded_bytes_U_AC,encoded_bytes_V_AC,
