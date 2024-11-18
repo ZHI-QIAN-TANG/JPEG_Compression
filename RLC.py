c6c66bba33f6051aba5cdb64c25a9030bf0d36b5
Shiba-Shi <120398106+Shiba-Shi@users.noreply.github.com>
2024-11-18 14:58:44 +0800

    add DownSample
    嘗試新增降裁量功能

--- RLC.py
+++ RLC.py
@@ -3,12 +3,19 @@
 def RLC(ACs):
     RL = []
     count = 0
+    
     for i in range(len(ACs)):
         if ACs[i] == 0:
             count += 1
         else:
+            while count > 15:  # 如果有超過15個0
+                RL.append([15, 0])  # 16個0視為一個ZRL
+                count -= 16
             RL.append([count, ACs[i]])
             count = 0
+    
+    # 結束時填加EOB標誌
     if count != 0:
-        RL.append([0,0])
+        RL.append([0, 0])
+    
     return RL
--- headertest.py
+++ headertest.py
@@ -9,7 +9,7 @@ def generate_jpeg_header(width, height, encoded_bytes):
     JFIF = b'JFIF\x00'  # JFIF標識符
     length = b'\x00\x10'  # APP0段長度
     version = b'\x01\x01'  # JFIF版本
-    units = b'\x01'  # 密度單位（0：無單位，1：每英寸，2：每厘米）
+    units = b'\x00'  # 密度單位（0：無單位，1：每英寸，2：每厘米）
     x_density = b'\x00\x48'  # X方向密度
     y_density = b'\x00\x48'  # Y方向密度
     x_thumb = b'\x00'  # 縮略圖寬度
@@ -25,7 +25,7 @@ def generate_jpeg_header(width, height, encoded_bytes):
 
     # 每個分量的信息（Y, Cb, Cr）
     components = (
-        b'\x01\x22\x00'  # Y分量（ID：1，取樣係數：1x1，量化表ID：0）
+        b'\x01\x11\x00'  # Y分量（ID：1，取樣係數：1x1，量化表ID：0）
         b'\x02\x11\x01'  # Cb分量（ID：2，取樣係數：1x1，量化表ID：1）
         b'\x03\x11\x01'  # Cr分量（ID：3，取樣係數：1x1，量化表ID：1）
     )
@@ -44,7 +44,7 @@ def generate_jpeg_header(width, height, encoded_bytes):
                 [72, 92, 95, 98, 112, 100, 103, 99] ]
     q_table_Y = bytes(z.Zigzag(table_Y))
 
-    # Define Quantization Table for Cb, Cr
+    # 定義量化表對於 Cb, Cr
     DQT_C = b'\xFF\xDB'  # Define Quantization Table
     dqt_length_C = b'\x00\x43'  # 段長度
     dqt_info_C = b'\x01'  # 表信息
@@ -57,35 +57,16 @@ def generate_jpeg_header(width, height, encoded_bytes):
                 [99, 99, 99, 99, 99, 99, 99, 99],
                 [99, 99, 99, 99, 99, 99, 99, 99]]
     q_table_C = bytes(z.Zigzag(table_C))
- 
-    def generate_dht_segment(huffman_data, table_class, table_id):
-        # Prepare DHT segment header
-        segment = b'\xFF\xC4'
-        segment_length = 2 + 1 + len(huffman_data)
-        segment += segment_length.to_bytes(2, byteorder='big')
-        # Table class (0 for DC, 1 for AC) and Huffman table index
-        table_class_index = (table_class << 4) | table_id
-        segment += bytes([table_class_index])
-        return segment 
+
+    # 定義DHT段（Huffman編碼表）
     y_dc_table = b'\xff\xc4\x00\x1f\x00\x00\x01\x05\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b'
     y_ac_table = b'\xff\xc4\x00\xb5\x10\x00\x02\x01\x03\x03\x02\x04\x03\x05\x05\x04\x04\x00\x00\x01}\x01\x02\x03\x00\x04\x11\x05\x12!1A\x06\x13Qa\x07"q\x142\x81\x91\xa1\x08#B\xb1\xc1\x15R\xd1\xf0$3br\x82\t\n\x16\x17\x18\x19\x1a%&\'()*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz\x83\x84\x85\x86\x87\x88\x89\x8a\x92\x93\x94\x95\x96\x97\x98\x99\x9a\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa'
-    uv_dc_table = b'\xff\xc4\x00\x1f\x01\x00\x03\x01\x01\x01\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b'
+    uv_dc_table = b'\xff\xc4\x00\x1f\x01\x00\x03\x01\x01\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b'
     uv_ac_table = b'\xff\xc4\x00\xb5\x11\x00\x02\x01\x02\x04\x04\x03\x04\x07\x05\x04\x04\x00\x01\x02w\x00\x01\x02\x03\x11\x04\x05!1\x06\x12AQ\x07aq\x13"2\x81\x08\x14B\x91\xa1\xb1\xc1\t#3R\xf0\x15br\xd1\n\x16$4\xe1%\xf1\x17\x18\x19\x1a&\'()*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x92\x93\x94\x95\x96\x97\x98\x99\x9a\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa'
-    #u_dc_table = generate_dht_segment(UV_DC_codebook_bytes, table_class=0, table_id=1)
-    #u_ac_table = generate_dht_segment(UV_AC_codebook_bytes, table_class=1, table_id=1)
-    #v_dc_table = generate_dht_segment(UV_DC_codebook_bytes, table_class=0, table_id=2)
-    #v_ac_table = generate_dht_segment(UV_AC_codebook_bytes, table_class=1, table_id=2)
-    '''
-    print("1:",y_dc_table,"\n")
-    print("1:",y_ac_table,"\n")
-    print("1:",u_ac_table,"\n")
-    print("1:",u_ac_table,"\n")
-    print("1:",v_dc_table,"\n")
-    print("1:",v_ac_table,"\n")
-    '''
+
     # 定義SOS段（Start Of Scan）
     SOS = b'\xFF\xDA'
-    sos_length = b'\x00\x0C'  # 段長度 or 0E
+    sos_length = b'\x00\x0C'  # 段長度
     num_sos_components = b'\x03'  # 分量數
     sos_components = (
         b'\x01\x00'  # Y分量
@@ -98,27 +79,18 @@ def generate_jpeg_header(width, height, encoded_bytes):
 
     # EOI標記（End of Image）
     EOI = b'\xFF\xD9'
-    print("EOI = ",EOI)
 
-    print()
     # 組裝JPEG標頭
     header = (
         SOI +
-        APP0 + length + JFIF + version + units + x_density + y_density + x_thumb + y_thumb +
+        APP0 + length + JFIF + version + units + x_density + y_density + x_thumb + y_thumb + 
         DQT_Y + dqt_length_Y + dqt_info_Y + q_table_Y +
         DQT_C + dqt_length_C + dqt_info_C + q_table_C +
         SOF0 + sof_length + precision + height_bytes + width_bytes + num_components + components +
         y_dc_table + y_ac_table + uv_dc_table + uv_ac_table +
         SOS + sos_length + num_sos_components + sos_components + start_spectral + end_spectral + approx_high +
         encoded_bytes +
-        # encoded_bytes_Y_DC + encoded_bytes_Y_AC + encoded_bytes_U_DC + encoded_bytes_U_AC + encoded_bytes_V_DC + encoded_bytes_V_AC + 
         EOI
     )
 
     return header
-
-
-
-# 用例
-#print(save_jpeg_header('headertmp.jpg', 600, 400,dc_jpeg_header, ac_jpeg_header, dc_merged_encoded_data, ac_merged_encoded_data))
-
